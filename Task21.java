155be8d81b4789ade95b004be457af180a664b58
l1s <dm.slisenko@gmail.com>
2017-03-17 10:48:39 +0300

    new part

---
+++ Task21.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+/*
+Для введённого пользователем с клавиатуры натурального числа посчитайте сумму всех
+его цифр (заранее не известно сколько цифр будет в числе).
+ */
+public class Task21 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите натуральное число:");
+        int n = sc.nextInt();
+        int sum = 0;
+
+        while (n != 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        System.out.println("Сумма цифр вашего числа = " + sum);
+    }
+}
