a7166ee6564480a7ba91441df61ff6ac3dbd24c1
l1s <dm.slisenko@gmail.com>
2017-03-19 18:45:29 +0300

    homework

--- lesson3/Task38.java
+++ lesson3/Task38.java
@@ -13,7 +13,7 @@ import java.util.Scanner;
 public class Task38 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = 0;
+        int n;
 
         while (true) {
             System.out.println("Введите чётное положительное число: ");
