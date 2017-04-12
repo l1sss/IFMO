7b85e5fb6e1e327ada81d2ae147ec2f7d71a09b9
l1s <dm.slisenko@gmail.com>
2017-04-12 12:06:10 +0300

    readWorks

--- lesson11/collections/ReadWords.java
+++ lesson11/collections/ReadWords.java
@@ -35,7 +35,7 @@ public class ReadWords {
         }
 
         //считаем кол-во слов в книге
-        System.out.println("ВСЕГО СЛОВ: " + words.size());
+        System.out.println("ВСЕГО СЛОВ В КНИГЕ: " + words.size());
 
         //топ n слов, которые встречаются чаще всего
         Map<String, Integer> topWords = new TreeMap<>();
@@ -74,13 +74,54 @@ public class ReadWords {
         topLength.sort(new Comparator<Map.Entry<Integer, Set<String>>>() {
             @Override
             public int compare(Map.Entry<Integer, Set<String>> o1, Map.Entry<Integer, Set<String>> o2) {
-                return -Integer.compare(o1.getKey(), o2.getKey());
+                return - Integer.compare(o1.getKey(), o2.getKey());
             }
         });
 
         System.out.println("\n ГРУППИРОВКА СЛОВ ПО ДЛИНЕ БУКВ: ");
         for (int i = 0; i < topLength.size(); i++) {
             System.out.println(topLength.get(i));
+        }
+
+        //топ букв в %
+        char[] alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+                                'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+        Map<Character, Double> letters = new TreeMap<>();
+        for (Character ch : alphabet) {
+            letters.put(ch, 0.0);
+        }
+
+        int lettersCount = 0;
+
+        for (String word : words) {
+            char[] charWord = word.toCharArray();
+            for (Character ch : charWord) {
+                Double count = letters.get(ch);
+
+                if (count == null) continue;
+
+                letters.put(ch, ++count);
+                lettersCount++;
+            }
+        }
+
+        System.out.println("\nВСЕГО БУКВ В КНИГЕ: " + lettersCount);
+
+        for (Map.Entry<Character, Double> pair : letters.entrySet()) {
+            Double count = pair.getValue();
+            pair.setValue(count / lettersCount * 100);
+        }
+
+        List<Map.Entry<Character, Double>> topLetters = new ArrayList<>(letters.entrySet());
+        topLetters.sort((o1, o2) -> - Double.compare(o1.getValue(), o2.getValue()));
+
+        System.out.println("\n ТОП БУКВ:\n");
+
+        for (Map.Entry<Character, Double> pair : topLetters) {
+            char letter = pair.getKey();
+            double count = pair.getValue();
+            System.out.print(letter + "= " + (int)count + "%;  ");
         }
     }
 }
